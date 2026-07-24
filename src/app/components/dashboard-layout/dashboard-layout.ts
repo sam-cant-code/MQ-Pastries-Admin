89d42407257547f7c411c8910b8ec4101857e935
sam-cant-code <135305742+sam-cant-code@users.noreply.github.com>
2026-07-24 21:59:11 +0530

    fixed mobile sidebar issue

--- src/app/components/dashboard-layout/dashboard-layout.ts
+++ src/app/components/dashboard-layout/dashboard-layout.ts
@@ -1,15 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './dashboard-layout.html',
 })
 export class DashboardLayout {
   private authService = inject(AuthService);
   private router = inject(Router);
+
+  isSidebarOpen = false;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 
   onSignOut(event: Event) {
     event.preventDefault();
