ddd2216daf564a4c81162115bf21537ae25ea769
sam-cant-code <135305742+sam-cant-code@users.noreply.github.com>
2026-07-28 08:15:58 +0530

    enviroment.ts changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'http://localhost:8080/api',
+  turnstileSiteKey: '1x00000000000000000000AA' // Cloudflare Official Testing Key (Always Passes)
 };
